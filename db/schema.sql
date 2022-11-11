5ab2e628e79ff1a64c8bae7926a19fd076e4cd69
nelson509 <35314794+nelson509@users.noreply.github.com>
2022-11-10 22:03:55 -0800

    read me link and seeds file

--- db/schema.sql
+++ db/schema.sql
@@ -12,9 +12,9 @@ CREATE TABLE departments (
 
 CREATE TABLE roles (
     id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
-    title VARCHAR(30) NOT NULL,
+    title VARCHAR(60) NOT NULL,
     salary DECIMAL (8,2) NOT NULL,
-    department_id INT NOT NULL 
+    department_id INT, 
     FOREIGN KEY (department_id)
     REFERENCES departments(id)
     ON DELETE SET NULL
@@ -24,8 +24,8 @@ CREATE TABLE employees (
     id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
     first_name VARCHAR(30) NOT NULL,
     last_name VARCHAR(30) NOT NULL,
-    role_id INT NOT NULL,
-    manager_id INT
+    role_id INT,
+    manager_id INT,
     FOREIGN KEY (role_id)
     REFERENCES roles(id)
     ON DELETE SET NULL
--- db/seeds.sql
+++ db/seeds.sql
@@ -1,27 +1,27 @@
 -- data to fill the 2 tables in the schema file
 
-INSERT INTO departments (name)
-VALUES ("Sales"),
-       ("Engineering"),
-       ("IT"),
-       ("HR")
-       ("Marketing")
+INSERT INTO departments (id, name)
+VALUES (1, "Sales"),
+       (2, "Engineering"),
+       (3, "IT"),
+       (4, "HR"),
+       (5, "Marketing");
 
-INSERT INTO roles (title, salary, department_id)
-VALUES ("Inside Sales Rep", 75,000.50, 1),
-       ("Web Developer", 105,000.25, 2),
-       ("Lead Engineer", 125,000, 2)
-       ("Help Desk Manager", 60,000.10, 3),
-       ("Human Resources Manager", 70,000.00, 4),
-       ("Junior Digital Marketing Manager", 75,000.75, 5)
+INSERT INTO roles (id, title, salary)
+VALUES (1, "Inside Sales Rep", 75000.50),
+       (2, "Web Developer", 105000.25),
+       (3, "Lead Engineer", 125000),
+       (4,"Help Desk Manager", 60000.10),
+       (5, "Human Resources Manager", 70000.00),
+       (6, "Junior Digital Marketing Manager", 75000.75);
 
-INSERT INTO employees (first_name, last_name, role_id, manager_id)
-                     ("Bob", "Jones", 11)
-                     ("Judy", "Smith", 12, 1)
-                     ("Bill", "Johnson", 13)
-                     ("Jane", "Wilson", 14)
-                     ("Tim", "Garcia", 15, 2)
-                     ("Molly", "Green", 16)
-                     ("Charles", "Brown", 17, 4)
-                     ("Jennifer", "Hill", 18, 1)
-                     ("June", "Park", 19, 9, 2)
+INSERT INTO employees (id, first_name, last_name, manager_id)
+VALUES               (1, "Bob", "Jones", 2),
+                     (2, "Judy", "Smith", 1),
+                     (3, "Bill", "Johnson", 3),
+                     (4, "Jane", "Wilson", 4),
+                     (5, "Tim", "Garcia", 2),
+                     (6, "Molly", "Green", 1),
+                     (7, "Charles", "Brown", 4),
+                     (8, "Jennifer", "Hill", 1),
+                     (9, "June", "Park", 2);
